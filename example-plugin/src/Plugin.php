9dafa2a9a63dc7cffaf8f2e20816a5c92b099146
Semir Mahovkic <semir.x64@gmail.com>
2019-05-29 11:56:18 +0200

    cleanup

--- example-plugin/src/Plugin.php
+++ example-plugin/src/Plugin.php
@@ -5,4 +5,14 @@ use PluginFactory\BasePlugin;
 
 class Plugin extends BasePlugin {
 
+    /**
+     * @override
+     * 
+     * Optionally, override default plugin_factory.yaml path
+     *
+     * @return  string  plugin_factory path
+     */
+    protected function pluginFactoryPath(): string {
+        return plugin_dir_path(dirname(__FILE__)) . 'src/plugin_factory.yaml';
+    }
 }
